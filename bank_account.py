1ccbfa51733cd35d1471450a0955e9cccc8e15ab
Alexander North <Koshmarvrn@gmail.com>
2025-09-29 20:23:00 +0300

    alpha version (fix bugs)

--- bank_account.py
+++ bank_account.py
@@ -32,6 +32,7 @@ class BankAccount:
         self.owner = owner # имя пользователя
         self.balance = type(self).quantize_money(balance) # баланс счёта
         self.currency = currency # баланс счёта
+        self.tz_info = timezone(timedelta(hours=3), name="UTC+3")
 
     def deposit(self, amount: Decimal): # функция внесения денег на счёт, в качестве аргументов необходимо передавать Decimal
         if not isinstance(amount, Decimal):
@@ -57,7 +58,9 @@ class BankAccount:
         return self.balance
 
     def _validate_withdraw(self, amount: Decimal, now: datetime | None):
-        pass
+        amount_q = type(self).quantize_money(amount)
+        if self.balance < amount_q:
+            raise InsufficientFunds('Недостаточно средств на счёте')
 
     @staticmethod
     def quantize_money(x: Decimal) -> Decimal:
@@ -74,9 +77,7 @@ class CheckingAccount(BankAccount):
         else:
             self.daily_withdraw_limit: Decimal = type(self).quantize_money(daily_withdraw_limit)
         self.withdrawn_today: Decimal = Decimal('0.00')
-        self.tz_info = timezone(timedelta(hours=3), name="UTC+3")
         self.window_date: date = datetime.now(self.tz_info).date()
-        self.allow_overdraft = True
 
     def _reset_window_if_need(self, now: date):
         if now != self.window_date:
@@ -94,66 +95,92 @@ class CheckingAccount(BankAccount):
             now = now.astimezone(self.tz_info)
 
         local_date = now.date()
-
         self._reset_window_if_need(local_date)
+
+        super()._validate_withdraw(amount_quantize, now)
+
+        if self.balance < amount_quantize:
+            raise InsufficientFunds('Недостаточно средств на счёте')
 
         if self.daily_withdraw_limit is not None:
             withdrawn_candidate = self.withdrawn_today + amount_quantize
             if withdrawn_candidate > self.daily_withdraw_limit:
                 raise LimitExceeded('Превышен суточный лимит снятий.')
-        self.withdrawn_today = type(self).quantize_money(self.withdrawn_today + amount_quantize)
+
+
+    def withdraw(self, amount: Decimal, *, now: datetime | None = None):
+        if not isinstance(amount, Decimal):
+            raise AccountError('В качестве суммы необходимо передавать тип Decimal')
+        if amount <= 0:
+            raise InvalidAmount('Сумма должна быть > 0')
+
+        self._validate_withdraw(amount, now)
+
+        amount_quantized = type(self).quantize_money(amount)
+
+        self.balance = type(self).quantize_money(self.balance - amount_quantized)
+
+        if self.daily_withdraw_limit is not None:
+            if now is None:
+                now = datetime.now(self.tz_info)
+            elif now.tzinfo is None:
+                now = now.replace(tzinfo=self.tz_info)
+            else:
+                now = now.astimezone(self.tz_info)
+            local_date = now.date()
+            self._reset_window_if_need(local_date)
+            self.withdrawn_today = type(self).quantize_money(self.withdrawn_today + amount_quantized)
+
+
 
 
 class SavingsAccount(BankAccount):
     def __init__(self, owner, balance: Decimal = Decimal('0.00'), currency='RUB',
-                 annual_rate: float = 0.00):
+                 annual_rate: Decimal = 0.00):
         super().__init__(owner, balance, currency)
-        self.annual_rate = float(annual_rate)
-        self.last_accrual_date: date = datetime.now().date()  # для этого поля достаточно будет date а не datetime
+        self.annual_rate = Decimal(str(annual_rate))
+        self.last_accrual_date: date = datetime.now(self.tz_info).date()  # для этого поля достаточно будет date а не datetime
 
     @staticmethod
     def _add_months(start: date, months: int):
         if months < 0:
             raise ValueError('Месяц должен быть больше или равен 0')
-        Y = start.year
-        M = start.month
-        D = start.day
-        total = (M - 1) + months
-        Y2 = Y + total // 12
-        M2 = (total % 12) + 1
 
-        if M2 in (1,3,5,7,8,10,12):
-            last_day = 31
-        elif M2 in (4,6,9,11):
-            last_day = 30
-        elif M2 == 2:
-            if Y2 % 400 == 0 or (Y2 % 4 == 0 and Y2 % 100 != 0):
-                last_day = 29
-            else:
-                last_day = 28
+        year = start.year + (start.month + months - 1) // 12
+        month = (start.month + months -1) % 12 + 1
 
-        D2 = min(D, last_day)
-        return date(Y2, M2, D2)
+        if month == 12:
+            next_month = date(year + 1, 1, 1)
+        else:
+            next_month = date(year, month + 1, 1)
+        last_day = (next_month - timedelta(days=1)).day
+
+        day = min(start.day, last_day)
+        return date(year, month, day)
+
 
     def accrue_monthly_interest(self, now: date):
         as_of = now
         start = self.last_accrual_date
-        month = (as_of.year - start.year) * 12 + (as_of.month - start.month)
+
+        total_month = (as_of.year - start.year) * 12 + (as_of.month - start.month)
+
         if as_of.day < start.day:
-            month -= 1
-        if month == 0:
+            total_month -= 1
+        if total_month == 0:
             return Decimal('0.00')
-        if month < 0:
-            month = 0
-        r_month_float = (1 + self.annual_rate) ** (1/12) - 1 # расчитываем месячную ставку
-        dec_r_month = Decimal(str(r_month_float))
-        balance_work = self.balance
+
+        r_month = (Decimal(1) + self.annual_rate) ** (Decimal(1) / Decimal(12)) - Decimal(1)  # расчитываем месячную ставку
+
         total_interest = Decimal('0.00')
-        for i in range(1,month+1):
-            interest_raw = balance_work * dec_r_month
-            interest = type(self).quantize_money(interest_raw)
-            balance_work = type(self).quantize_money(balance_work + interest)
+        balance_work = self.balance
+
+        for month in range(total_month):
+            interest = type(self).quantize_money(balance_work * r_month)
             total_interest = type(self).quantize_money(total_interest + interest)
+            balance_work = type(self).quantize_money(balance_work + interest)
+
         self.balance = balance_work
-        self.last_accrual_date = type(self)._add_months(self.last_accrual_date, month)
+        self.last_accrual_date = as_of
+
         return total_interest
