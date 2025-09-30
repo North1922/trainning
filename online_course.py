476306e244532e0e604a2c8461b1c5c3a4d8d62c
Alexander North <Koshmarvrn@gmail.com>
2025-09-30 23:18:15 +0300

    init online_course.py

---
+++ online_course.py
@@ -0,0 +1,29 @@
+#TODO Задание 2. Онлайн-курсы
+#TODO Сделай систему управления курсами.
+#TODO Класс Course (название, преподаватель, список студентов).
+
+#TODO Класс Student (имя, email, список курсов).
+
+#TODO Класс Teacher (имя, специализация).
+# Методы: add_student(), remove_student(), list_students().
+
+#TODO  Вопрос для размышления: кто хранит список студентов — курс или студент? А кто хранит список курсов?
+
+class Course:
+    pass
+
+class Student:
+    pass
+
+
+class Teacher:
+    pass
+
+    def add_student(self):
+        pass
+
+    def remove_student(self):
+        pass
+
+    def list_students(self):
+        pass
